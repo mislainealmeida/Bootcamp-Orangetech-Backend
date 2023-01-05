37c6c0cd292494c1bfbc9c8488104a77786f04b2
Mis <a.s.mislaine@gmail.com>
2023-01-05 10:20:19 -0300

    DesafioBasico2

--- desafios/basico/Desafio02.java
+++ desafios/basico/Desafio02.java
@@ -5,44 +5,15 @@ import java.util.Scanner;
 public class Desafio02 {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
-        //System.out.println("Digite a porcentagem, usando 1-10");
-        //System.out.println("10% = 1");
+        //System.out.println("Digite entre 1-10");
         int tamanho = leitor.nextInt();
 
-        String resultado;
-        if(tamanho==1){
-            resultado= "/";
+        if(tamanho > 0 && tamanho <=10){
+            for (int i = 0; i< tamanho; i++){
+                System.out.print("/");
+            }
+        }else{
+            System.out.println("Valor inválido. Digite entre 1-10");
         }
-        else if(tamanho==2){
-            resultado = "//";
-        }
-        else if(tamanho==3){
-            resultado = "///";
-        }
-        else if(tamanho==4){
-            resultado = "////";
-        }
-        else if(tamanho==5){
-            resultado = "/////";
-        }
-        else if(tamanho==6){
-            resultado = "//////";
-        }
-        else if(tamanho==7){
-            resultado = "///////";
-        }
-        else if(tamanho==8){
-            resultado = "////////";
-        }
-        else if(tamanho==9){
-            resultado = "/////////";
-        }
-        else if(tamanho==10){
-            resultado = "//////////";
-        }
-        else{
-            resultado = "Valor inválido. Digite um número entre 1-10";
-        }
-        System.out.println(resultado);
     }
 }
